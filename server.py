7229e934add593a1ff016ee268ad29c0448820c1
SoloLeveling94 <sbokby@gmail.com>
2022-02-21 09:07:14 +0100

    updated tests and bug 12 places

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 from flask import Flask, render_template, request, redirect, flash, url_for
 
 
@@ -19,7 +20,7 @@ app.secret_key = 'something_special'
 
 competitions = loadCompetitions()
 clubs = loadClubs()
-
+dict_points = {c['name']: 12 for c in competitions}
 
 @app.route('/')
 def index():
@@ -30,10 +31,13 @@ def index():
 def showSummary():
     try:
         club = [club for club in clubs if club['email'] == request.form['email']][0]
+        # date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
     except IndexError:
         flash("Your email is not registered!")
         return redirect(url_for('index'))
-    return render_template('welcome.html', club=club, competitions=competitions)
+    return render_template('welcome.html', date=date, club=club,
+                           competitions=competitions)
 
 
 @app.route('/book/<competition>/<club>')
@@ -41,7 +45,7 @@ def book(competition, club):
     foundClub = [c for c in clubs if c['name'] == club][0]
     foundCompetition = [c for c in competitions if c['name'] == competition][0]
     if foundClub and foundCompetition:
-        return render_template('booking.html', club=foundClub, competition=foundCompetition)
+        return render_template('booking.html', club=foundClub, competition=foundCompetition, dict_points=dict_points)
     else:
         flash("Something went wrong-please try again")
         return render_template('welcome.html', club=club, competitions=competitions)
@@ -51,26 +55,40 @@ def book(competition, club):
 def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
+    # date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     try:
         placesRequired = int(request.form['places'])
-        if placesRequired > int(club['points']):
-            flash("You don't have enough points to purchase this places!")
-            return render_template('booking.html', club=club, competition=competition)
-        if 0 < placesRequired <= 12:
-            if int(competition['numberOfPlaces']) < placesRequired:
-                flash("You can't purchase more than the number of places available!")
-                return render_template('booking.html', club=club, competition=competition)
+        numberPlaceCompetitionByClub = dict_points.get(competition['name'])
 
+        if placesRequired > int(club['points']):
+            flash("You do not have enough points to purchase this places!")
+            return render_template('booking.html', dict_points=dict_points, club=club, competition=competition)
+
+        if int(competition['numberOfPlaces']) < placesRequired:
+            flash("You can not purchase more than the number of places available!")
+            return render_template('booking.html', dict_points=dict_points, club=club, competition=competition)
+
+        if placesRequired < 1:
+            flash("Enter a positive number to book it!")
+            return render_template('booking.html', dict_points=dict_points, club=club, competition=competition)
+
+        if placesRequired <= numberPlaceCompetitionByClub:
             competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
+            dict_points.update({competition['name']: numberPlaceCompetitionByClub - placesRequired})
+            club['points'] = int(club['points']) - placesRequired
             flash('Great-booking complete!')
-            return render_template('welcome.html', club=club, competitions=competitions)
+            return render_template('welcome.html', dict_points=dict_points, club=club,
+                                   competitions=competitions,)
+            # return render_template('welcome.html', dict_points=dict_points, date=date, club=club,
+            #                        competitions=competitions,)
         else:
-            flash('You can book between 1 and 12 places max in a competition!')
-            return render_template('booking.html', club=club, competition=competition)
+            flash('You can only book 12 places max for this competition')
+            return render_template('booking.html', dict_points=dict_points, club=club, competition=competition)
+
     except ValueError:
         flash("Enter a number!")
-        return render_template('booking.html', club=club, competition=competition)
+        return render_template('booking.html', dict_points=dict_points, club=club, competition=competition)
 
 
 # TODO: Add route for points display
--- tests/test_app.py
+++ tests/test_app.py
@@ -16,28 +16,37 @@ def test_should_status_code_ok(test_client):
 def test_login_with_registered_email(test_client):
     response = test_client.post('/showSummary', data={"email": "john@simplylift.co"}, follow_redirects=True)
     assert response.status_code == 200
-
+    assert b"Welcome, john@simplylift.co" in response.data
 
 def test_login_with_unregistered_email(test_client):
     response = test_client.post('/showSummary', data={"email": "test@xyz.com"}, follow_redirects=True)
     assert response.status_code == 200
+    assert b"Your email is not registered!" in response.data
 
 
 def test_no_enough_points_available(test_client):
     response = test_client.post('/purchasePlaces', data={"club": "Iron Temple", "competition": "Spring Festival",
                                                          "places": 8}, follow_redirects=True)
     assert response.status_code == 200
+    assert b"You do not have enough points to purchase this places!" in response.data
 
 
 def test_outnumbered_places_required(test_client):
-    response = test_client.post('/purchasePlaces', data={"club": "Simply Lift", "competition": "Spring Festival",
+    response = test_client.post('/purchasePlaces', data={"club": "Simply Lift", "competition": "Spring Festival 2022",
                                                          "places": 13}, follow_redirects=True)
     assert response.status_code == 200
+    assert b"You can not purchase more than the number of places available!" in response.data
 
 
 def test_negative_places_required(test_client):
     response = test_client.post('/purchasePlaces', data={"club": "Simply Lift", "competition": "Spring Festival",
                                                          "places": -5}, follow_redirects=True)
     assert response.status_code == 200
+    assert b"Enter a positive number to book it!" in response.data
 
 
+def test_max_12_places_competition(test_client):
+    response = test_client.post('/purchasePlaces', data={"club": "Simply Lift", "competition": "Spring Festival",
+                                                         "places": 13}, follow_redirects=True)
+    assert response.status_code == 200
+    assert b"You can only book 12 places max for this competition" in response.data
